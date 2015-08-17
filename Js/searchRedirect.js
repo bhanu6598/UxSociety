c69e19c163f8a67d35fc5eb62ff92dc93ab349af
bhanu6598 <bhanu6598@Bhanus-MacBook-Pro.local>
2015-08-16 23:42:34 -0700

    Removing previos version

--- Js/searchRedirect.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by bhanu6598 on 8/14/15.
- */
-(function(){
-    document.getElementById('my_button').onclick = function() {
-        document.getElementById('searchBar').scrollIntoView();
-        document.getElementById('searchBar').focus();
-    };
-})();
